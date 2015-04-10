417f234e803930afb9ad0687e9f2f228f17db01f
jake <jake@touchmd.com>
2015-04-09 22:48:39 -0600

    Sets up scaffolding for the quiz cyborg.

---
+++ quiz_cyborg.js
@@ -0,0 +1,22 @@
+// quiz cyborg
+
+// TODO: Make a friggin timer.
+var timer;
+
+// Randy returns a random number between 25000 and 10000. Ultimately these will be milliseconds.
+var randy = function() {
+    return Math.floor(Math.random() * (25000 - 10000 + 1)) + 10000;
+};
+
+// Setup
+var setupCyborg = function(){
+    // TODO: This will check for jquery, and for answer key, etc.
+    // TODO: If there is no answer key, we should end execution.
+
+
+};
+
+// Main
+var Main = function(){
+    // TODO: Main program execution.
+};
