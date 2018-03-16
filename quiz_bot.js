148bc27c79d43b52b2e84e311e7136f130edbad4
Jacob Graves <freakazoidjake@gmail.com>
2018-03-15 22:54:28 -0600

    Formatting.

--- quiz_bot.js
+++ quiz_bot.js
@@ -1,59 +1,56 @@
 // quiz bot
-var retrieveAnswerKey = function(){
+var retrieveAnswerKey = function () {
 	return JSON.parse(localStorage.getItem('answerKey'));
 }
-var localStorageExists = function(){
+var localStorageExists = function () {
 	var a_key = retrieveAnswerKey();
-	if(typeof a_key !== "undefined" && a_key && a_key.length>0){
+	if (typeof a_key !== "undefined" && a_key && a_key.length > 0) {
 		return true;
-	} else{
+	} else {
 		return false;
 	}
 }
 
 var answer_key = localStorageExists() ? retrieveAnswerKey() : [];
 
-var wrongAnswer = function(qid, aid){
-	for(var i=0; i<answer_key.length; i++){
+var wrongAnswer = function (qid, aid) {
+	for (var i = 0; i < answer_key.length; i++) {
 		var q = answer_key[i];
-		if(qid == q.q_id){
+		if (qid == q.q_id) {
 			return q.incorrects.indexOf(aid) > -1;
 		}
 	}
 	return false;
 };
 
-var qidInAnswerkey = function(qid){
-	for(var i=0; i<answer_key.length; i++){
+var qidInAnswerkey = function (qid) {
+	for (var i = 0; i < answer_key.length; i++) {
 		var q = answer_key[i];
-		if(qid == q.q_id){
+		if (qid == q.q_id) {
 			return true;
 		}
 	}
 	return false;
 };
 
-function runBot(options){ // add 'practice_test': boolean
-	$('.display_question').each(function(index, value){
+function runBot(options) { // add 'practice_test': boolean
+	$('.display_question').each(function (index, value) {
 		var question_id = $(value).attr('id').split('_')[1],
-			question_text = $(value).find('.question_'+question_id+'question_text').text().trim();
-		if(options['first_time'] === true){
+			question_text = $(value).find('.question_' + question_id + 'question_text').text().trim();
+		if (options['first_time'] === true) {
 			// console.log('FIRST TIME THROUGH');
 			var input = $(value).find('.answer_input > input')[0];
-			if(input) {
+			if (input) {
 				var answer_id = input.value;
-				$('#question_'+question_id+'_answer_'+answer_id).click().attr('checked','checked');
+				$('#question_' + question_id + '_answer_' + answer_id).click().attr('checked', 'checked');
 			} else {
 				//We have a matching choice!
-				$(value).find('.answer > div > select').each(function(index, select){
+				$(value).find('.answer > div > select').each(function (index, select) {
 					var firstOption = $(select).find('option')[1].value; //index 0 is always '[ Choose ]'
 					$(select).val(firstOption).change();
 				});
-				
 			}
-			
-			
-		} else if(qidInAnswerkey(question_id)){
+		} else if (qidInAnswerkey(question_id)) {
 			// console.log('QUESTION ID IS IN ANSWER KEY');
 			for(var i=0; i<answer_key.length; i++){
 				if(question_id == answer_key[i].q_id){
@@ -72,18 +69,19 @@ function runBot(options){ // add 'practice_test': boolean
 						});
 					}
 				}
-			}	
-		} else if(options['practice_test'] === true){
+
+			}
+		} else if (options['practice_test'] === true) {
 			console.log('answer this one yourself: it\'s blank');
-		} else{
 			var answer_id = $(this).find('.answer_input > input')[0].value;
 			$('#question_'+question_id+'_answer_'+answer_id).click().attr('checked','checked');
+		} else {
 		}
 	});
 }
 
-if(answer_key.length == 0){
-	runBot({'first_time': true, 'practice_test': true});
-} else{
-	runBot({'first_time': false, 'practice_test': false});
+if (answer_key.length == 0) {
+	runBot({ 'first_time': true, 'practice_test': true });
+} else {
+	runBot({ 'first_time': false, 'practice_test': false });
 }
