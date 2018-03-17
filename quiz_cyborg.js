d35db7a03b54d0fc35b6ca25c1621131d586bf06
Jacob Graves <jacobgraves@students.suu.edu>
2018-03-16 19:01:20 -0600

    Adds jquery stuff.

--- quiz_cyborg.js
+++ quiz_cyborg.js
@@ -2,6 +2,21 @@
 
 // TODO: Make a friggin timer.
 var timer;
+var checkForJquery = function () {
+    if (window.jQuery) {
+        console.log("Your jQuery version is:", jQuery.fn.jquery);
+    } else {
+        injectJquery();
+    }
+};
+
+var injectJquery = function () {
+    var script = document.createElement('script');
+    script.type = "text/javascript";
+    script.src = 'https://code.jquery.com/jquery-latest.min.js';
+    var head = document.getElementsByTagName('head')[0];
+    head.appendChild(script);
+};
 
 // Randy returns a random number between 25000 and 10000. Ultimately these will be milliseconds.
 var randy = function() {
